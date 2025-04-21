8267214910a83ce4a4bd1235604810df0006539a
LeiterStosszahn <129488766+LeiterStosszahn@users.noreply.github.com>
2025-04-21 12:34:03 +0800

    Delete crawler/apiKey.py

--- crawler/apiKey.py
+++
@@ -1,7 +0,0 @@
-HEADERS = {
-    "Content-type": "application/json; charset=UTF-8",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
-    "Accept": "application/json",
-}
-
-MB = 1024**2
